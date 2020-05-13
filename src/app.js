a94e58296807cf08576b6e0ae572c9b5ed037669
Odinaka Joy <dinakajoy@gmail.com>
2020-05-13 09:23:37 +0100

    correct local development port issue

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome To Team-048 eTeachersRecruit Application</h1>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Welcome To Team-048 eTeachersRecruit Application</h1>
+    </header>
   );
+}
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <small>Developed By Team-048 As #BuildForSDG Project</small>
+    </footer>
+  );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="main" />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello there
+      </div>
+    );
+  }
+}
+
+export default HomePage;
