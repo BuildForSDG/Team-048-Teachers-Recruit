8bccd1b7b7058335ff87f9b310ebd16ea9ca2da2
Odinaka Joy <dinakajoy@gmail.com>
2020-05-15 06:48:33 +0100

    feat: create the homepage of the application Fixes #1

--- src/app.js
+++ src/app.js
@@ -1,19 +1,43 @@
+/* eslint-disable max-len */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './App.css';
+import HomePage from './components/Pages/HomePage';
+import GuestNavbar from './components/Navbar/GuestNavbar';
+import logo from './images/logo.png';
 
 function Header() {
   return (
     <header className="App-header">
-      <h1>Welcome To Team-048 eTeachersRecruit Application</h1>
+      <GuestNavbar />
     </header>
   );
 }
 
 function Footer() {
   return (
-    <footer className="App-footer">
-      <small>Developed By Team-048 As #BuildForSDG Project</small>
+    <footer>
+      <div className="footer">
+        <div className="footer-one">
+          <img src={logo} alt="eRecriut" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem modi eum aliquam consequuntur aitecto deserunt</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem modi eum aliquam consequuntur aitecto deserunt</p>
+        </div>
+        <div className="footer-two">
+          <h3>LINKS:</h3>
+          <p>Home</p>
+          <p>About</p>
+          <p>Vacancies</p>
+          <p>Contact</p>
+        </div>
+        <div className="footer-three">
+          <h3>OPENINGS:</h3>
+          <p>Basic Three Teacher</p>
+          <p>Basic Three Teacher</p>
+          <p>Basic Three Teacher</p>
+        </div>
+      </div>
+      <div className="small">Developed By Team-048 As #BuildForSDG Project</div>
     </footer>
   );
 }
@@ -23,7 +47,7 @@ class App extends React.Component {
     return (
       <>
         <Header />
-        <main className="main" />
+        <HomePage />
         <Footer />
       </>
     );
